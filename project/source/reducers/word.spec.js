e3e97c0554bb4fe6a033dfffd05ab7d260332f8d
Peter Orum <peter.orum@razorfish.com>
2016-12-16 11:28:50 +1100

    use chai

--- project/source/reducers/word.spec.js
+++ project/source/reducers/word.spec.js
@@ -8,10 +8,23 @@ describe( 'Word reducer', function() {
     const action = {
       type: 'UPDATE_WORD'
     };
+
     const actual = word( [], action );
 
     expect( actual ).to.be.a( 'string' );
     expect( actual.length ).to.be.above( 0 );
+  } );
+
+  it( 'return a different word', function() {
+
+    const action = {
+      type: 'UPDATE_WORD'
+    };
+
+    const word1 = word( [], action );
+    const word2 = word( [], action );
+
+    expect( word1 ).to.not.equal( word2 );
   } );
 
 } )
